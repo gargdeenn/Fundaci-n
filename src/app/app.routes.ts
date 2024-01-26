92290104aa511993b9556dbed2f33d5bf1532979
gargdeenn <dandressantiago@unicesar.edu.co>
2024-01-26 13:25:54 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ActivitiesComponent } from './activities/activities.component';
+import { ContactComponent } from './contact/contact.component';
+import { NoticesComponent } from './notices/notices.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'about',
+        component: AboutComponent
+    },
+    {
+        path: 'activities',
+        component: ActivitiesComponent
+    },
+    {
+        path: 'contact',
+        component: ContactComponent
+    },
+    {
+        path: 'notice',
+        component: NoticesComponent
+    }
+];
